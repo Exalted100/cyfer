95bc9731c297e2ddf9e19dfbb371f0d8e2d82969
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-29 23:52:05 +0100

    first commit

---
+++ middleware/auth.ts
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+type nextReturnType = {
+    done: boolean
+}
+
+const auth = (req: any, res: any, next: () => nextReturnType) => {
+    try {
+        const token = req.header("Authorization")
+        if(!token) return res.status(400).json({msg: "Invalid Authentication."})
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: {}, user: {}) => {
+            if(err) return res.status(400).json({msg: "Invalid Authentication."})
+
+            req.user = user
+            next()
+        })
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+}
+
+export default auth
---
+++ models/userModel.ts
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please enter your first name!"],
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please enter your last name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        trim: true,
+        unique: true
+    },
+    notes: [{
+        title: String,
+        note: String,
+        password: String,
+        dateCreated: Date
+    }],
+    password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    }
+})
+
+export default mongoose.model("Users", userSchema)
